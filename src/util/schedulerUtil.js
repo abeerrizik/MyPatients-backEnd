33454afb8e46597f4f5266231ff9ef13b5ecf7ad
Moris R <moris.rafol@gmail.com>
2020-12-04 21:09:13 +0200

    feat: add scheduler dependencies

--- src/util/schedulerUtil.js
+++ src/util/schedulerUtil.js
@@ -2,21 +2,18 @@ const treatmentsModel = require("../models/treatments")
 const nurseModel = require("../models/nurses")
 const scheduler = require('node-schedule');
 const {pushNotification} = require("./notificationsManager")
-exports.loadAllSchedules = async ()=>{
+exports.loadAllSchedules = async () => {
     const treatments = await treatmentsModel.getAllTreatments()
     const today = Date.now()
 
     treatments
-        .filter(data=> data.Time > today )
-        .forEach(data=>{
-            scheduler.scheduleJob(data.Time,()=>{
-            nurseModel.getDataById(data["Nurse id"]).then((nurseData)=> {
-                pushNotification(nurseData.subscription, "this is a test")
+        .filter(data => data.Time > today)
+        .forEach(treatmentData => {
+            scheduler.scheduleJob(treatmentData.Time, () => {
+                nurseModel
+                    .getDataById(treatmentData["Nurse id"])
+                    .then((nurseData) =>
+                        pushNotification(nurseData.subscription, `it's treatment time, at room ${data.Room} `))
             })
         })
-    })
-
-
-    // treatments = treatments.map(data=> ({...data, Time: dayJs(today).se }))
-
 }
